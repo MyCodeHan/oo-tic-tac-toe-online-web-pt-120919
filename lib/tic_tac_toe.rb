09d195c8d56a5a86003c537e7c3d40acd5f58bd5
Hannah Harrington <hannahharrington@outlook.com>
2020-01-01 18:46:54 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,7 +1,7 @@
 class TicTacToe
 
   def initialize(board = nil)
-    # @board = board || Array.new(9, " ")
+     @board = board || Array.new(9, " ")
   end
 
   def play
@@ -132,7 +132,7 @@ class TicTacToe
       nil
     end
   end
-
+end
 
 
 
